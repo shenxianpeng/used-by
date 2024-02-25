922cc807f3792ef4ab151eeaa7fcd0f6718b3ab9
shenxianpeng <xianpeng.shen@gmail.com>
2024-02-25 21:47:37 +0800

    save changes

--- used_by/main.py
+++ used_by/main.py
@@ -14,9 +14,32 @@ def get_parser():
     return parser
 
 
-def get_dependents_info(url: str) -> list:
+def get_soup(url: str):
     r = requests.get(url=url)
     soup = BeautifulSoup(r.content, "html.parser")
+    return soup
+
+
+def has_next_page(soup) -> bool:
+    '''identify if there is next button'''
+    paginate_container = soup.find("div", {"class": "paginate-container"}).find('a')
+    if paginate_container:
+       return True
+    else:
+        return False
+
+def process_pagination(soup):
+    '''get and return next page soup information.'''
+    paginate_container = soup.find("div", {"class": "paginate-container"}).find('a')
+    if paginate_container:
+        href = paginate_container["href"]
+        print(href) # next page url
+        return get_soup(url=href)
+    return None
+
+
+def get_dependents_info(url: str) -> list:
+    soup = get_soup(url=url)
 
     # identify there is menu list or not
     menu_list = soup.find_all('a', class_='select-menu-item')
@@ -27,9 +50,7 @@ def get_dependents_info(url: str) -> list:
 
     for m_list in menu_list:
         href = m_list['href']
-
-        r = requests.get(url=f"https://github.com{href}")
-        soup = BeautifulSoup(r.content, "html.parser")
+        soup = get_soup(url=f"https://github.com{href}")
 
         page_data = [
             "{}/{}".format(
@@ -50,6 +71,9 @@ def get_dependents_info(url: str) -> list:
                 total_star += int(span_tag.text.strip().replace(',', ''))
             if svg_fork_tag:
                 total_fork += int(span_tag.text.strip().replace(',', ''))
+
+        while has_next_page(soup):
+            soup = process_pagination(soup)
 
     print(f"total_number is {total_number}")
     print(f"total_star is {total_star}")
