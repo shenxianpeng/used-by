1be8f0995d1fc76c91d3527036767e4e3d69c053
shenxianpeng <xianpeng.shen@gmail.com>
2024-02-29 17:41:05 +0800

    feat: get total deps number

--- used_by/main.py
+++ used_by/main.py
@@ -10,7 +10,6 @@ def get_parser():
         '--repo',
         help='GitHub repository name. e.g. cpp-linter/cpp-linter-action'
     )
-
     return parser
 
 
@@ -18,59 +17,6 @@ def get_soup(url: str):
     r = requests.get(url=url)
     soup = BeautifulSoup(r.content, "html.parser")
     return soup
-
-
-def has_next_page(soup) -> bool:
-    '''identify if there is next button'''
-    paginate_container = soup.find("div", {"class": "paginate-container"}).find('a', text='Next')
-    if paginate_container:
-       return True
-    else:
-        return False
-
-
-def get_page_number(soup):
-    page_data = [
-        "{}/{}".format(
-            t.find('a', {"data-repository-hovercards-enabled":""}).text,
-            t.find('a', {"data-hovercard-type":"repository"}).text
-        )
-        for t in soup.findAll("div", {"class": "Box-row"})
-    ]
-
-    return len(page_data)
-
-
-def get_star_number(soup):
-    span_tags = soup.find_all('span', class_='color-fg-muted text-bold pl-3')
-    total_star = 0 
-    for span_tag in span_tags:
-        svg_star_tag = span_tag.find('svg', class_='octicon octicon-star')
-        if svg_star_tag:
-            total_star += int(span_tag.text.strip().replace(',', ''))
-
-    return total_star
-
-
-def get_fork_number(soup):
-    span_tags = soup.find_all('span', class_='color-fg-muted text-bold pl-3')
-    total_fork = 0 
-    for span_tag in span_tags:
-        svg_fork_tag = span_tag.find('svg', class_='octicon octicon-repo-forked')
-        if svg_fork_tag:
-            total_fork += int(span_tag.text.strip().replace(',', ''))
-
-    return total_fork
-
-
-def process_pagination(soup):
-    '''get and return next page soup information.'''
-    paginate_container = soup.find("div", {"class": "paginate-container"}).find('a', text='Next')
-    if paginate_container:
-        href = paginate_container["href"]
-        print(href) # next page url
-        return get_soup(url=href)
-    return None
 
 
 def get_dependents_info(url: str) -> list:
@@ -84,20 +30,15 @@ def get_dependents_info(url: str) -> list:
     for m_list in menu_list:
         href = m_list['href']
         soup = get_soup(url=f"https://github.com{href}")
-        
-        total_number += get_page_number(soup)
-        total_star += get_star_number(soup)
-        total_fork += get_fork_number(soup)
 
-        while has_next_page(soup):
-            soup = process_pagination(soup)
-            total_number += get_page_number(soup)
-            total_star += get_star_number(soup)
-            total_fork += get_fork_number(soup)
+        repo_text = soup.find('a', class_='btn-link selected').get_text(strip=True)
 
-    print(f"total_number is {total_number}")
-    print(f"total_star is {total_star}")
-    print(f"total_fork is {total_fork}")
+        # Extract the number of repositories
+        repo_number = int(repo_text.split()[0])
+
+        total_number += repo_number
+
+    return total_number
 
 
 def main():
@@ -106,7 +47,8 @@ def main():
 
     url = f"https://github.com/{args.repo}/network/dependents"
     
-    get_dependents_info(url)
+    result = get_dependents_info(url)
+    print(result)
 
 if __name__ == "__main__":
     main()
